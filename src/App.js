195a1d0d838148c3f31792bf240500bdb815fbb2
ashut5959 <ashutoshpal59@gmail.com>
2023-05-02 13:17:31 +0530

    (updated)

--- src/App.js
+++ src/App.js
@@ -1,66 +1,64 @@
 // import logo from './logo.svg';
-import { useState } from 'react';
-// import { Switch, Route } from "react-router-dom";
-import './App.css';
-import Alert from './Components/Alert';
-// import About from './Components/About';
-import Navbar from './Components/Navbar';
-import TextForm from './Components/TextForm';
-
+import { useState } from "react";
+import Alert from "./Components/Alert";
+import About from "./Components/About";
+import Navbar from "./Components/Navbar";
+import TextForm from "./Components/TextForm";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
 
-  const [mode , setMode]  = useState('light');
-  const [alert , setAlert] = useState(null);
-
-  const showAlert = (message,type) => {
-      setAlert(
-        {
-          msg: message,
-          type : type
-        }
-      )
-      setTimeout(()=> {
-        setAlert(null);
-       } , 3000
-      )
-  }
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 3000);
+  };
 
   const toggleMode = () => {
-    if(mode === 'light')
-    {
-      setMode('dark');
-      document.body.style.backgroundColor = '#220d5a';
-      showAlert("Dark Mode is enabled" , "success");
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#220d5a";
+      showAlert("Dark Mode is enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode is enabled", "success");
     }
-    else
-    {
-      setMode('light')
-      document.body.style.backgroundColor = 'white';
-      showAlert("Light Mode is enabled" , "success")
-  }
-    
-  }
+  };
 
   return (
     <>
-        {/*<Router>*/}
-                <Navbar title = "TEXTUTELS" aboutus = "About TextUtels" mode={mode} toggleMode = {toggleMode}/>
-                <Alert alert = {alert}/>
-                <div className='container my-3'>
-          
-                {/*<Routes>
-  <Route exact path="/about" element={<About />}/>
-                  
-                
-                  <Route exact path="/" element={}/>
-                    
-                </Routes>*/}
-          
-                <TextForm showAlert = {showAlert} heading = 'Enter the statement' mode={mode} />
-          </div>
-          {/*</Router>*/}
-          
+      <Router>
+        <Navbar
+          title="TEXTUTELS"
+          aboutus="About TextUtels"
+          mode={mode}
+          toggleMode={toggleMode}
+        />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <TextForm
+                  showAlert={showAlert}
+                  heading="Enter the statement"
+                  mode={mode}
+                />
+              }
+            />
+            <Route exact path="/about" element={<About mode={mode} />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -39,9 +39,7 @@ export default function TextForm(props) {
        
     } 
     const CopyT = () => {
-      var text = document.getElementById('myBox');
-      text.select();
-      navigator.clipboard.writeText(text.value);
+      navigator.clipboard.writeText(text);
       props.showAlert("Text copied" , "success")
     }
 
@@ -73,23 +71,23 @@ export default function TextForm(props) {
                 <div className="container my-3"  style={{color: props.mode === 'dark' ? 'white' : 'black'}} >
                         <h1>{props.heading}</h1>
                         <div className="mb-3">
-                        <textarea className="form-control" id="myBox" style={{backgroundColor: props.mode === 'dark' ? 'grey' : 'white'}} value = {text} onChange = {handleOnChange} rows="8"></textarea>
+                        <textarea className="form-control" id="myBox" style={{backgroundColor: props.mode === 'dark' ? '#2a5174' : 'white'}} value = {text} onChange = {handleOnChange} rows="8"></textarea>
                         </div>
                         
-                        <button className="btn btn-primary mx-1" onClick={Upper}>Convert to UpperCase</button>
-                        <button className="btn btn-primary mx-1" onClick={Lower}>Convert to LowerCase</button>
-                        <button className="btn btn-primary mx-1" onClick={Clear}>Clear</button>
-                        <button className="btn btn-primary mx-1" onClick={email}>Email Extrat</button>
-                        <button className="btn btn-primary mx-1" onClick={replaceString}>ReplaceString</button>
-                        <button className="btn btn-primary mx-1" onClick={CopyT}>Copy Text</button>
-                        <button className="btn btn-primary mx-1" onClick={extraSpace}>Remove Extra Space</button>
+                        <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={Upper}>Convert to UpperCase</button>
+                        <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={Lower}>Convert to LowerCase</button>
+                        <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={Clear}>Clear</button>
+                        <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={email}>Email Extrat</button>
+                        <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={replaceString}>ReplaceString</button>
+                        <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={CopyT}>Copy Text</button>
+                        <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={extraSpace}>Remove Extra Space</button>
             </div>
             <hr />
             <div className="container my-3" style={{color: props.mode === 'dark' ? 'white' : 'black'}} >
 
                 <h1>Your Text Summary</h1>
-                <p>{text.split(" ").length} words {text.length} characters</p>
-                <p>{.008 * text.split(" ").length} Minutes to read</p>
+                <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words {text.length} characters</p>
+                <p>{.008 * text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Minutes to read</p>
                 <h2>PREVIEW</h2>
                 <p>{text.length > 0 ? text : 'Enter something in the text box to preview here'}</p>
                 <h1>Emails</h1>
